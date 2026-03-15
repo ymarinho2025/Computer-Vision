1fb50eb174eac6d2e4ffe81c5fac80c8a6d45334
Yuri Marinho <ymarinho2025@gmail.com>
2026-03-15 16:43:05 -0300

    Comentarios adicionados

--- detectar-rostos/main.py
+++ detectar-rostos/main.py
@@ -1,17 +1,20 @@
 import cv2
 
+# carregar direto da lib do opencv, não precisa baixar o xml
 carregaAlgoritmo = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 )
 
-image = cv2.imread('fotos/image-4.png')
+image = cv2.imread('fotos/image-4.jpg')
 
+# imagens cinzas são mais fáceis de processar, por isso convertemos a imagem para cinza
 imagemCinza = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 faces = carregaAlgoritmo.detectMultiScale(imagemCinza)
 
 print(faces)
 
+# (x para horizontal e y para vertical) (l para largura, a para altura)
 for(x, y, l, a) in faces:
     cv2.rectangle(image, (x, y), (x+l, y+a), (0, 255, 0), 2)
 
